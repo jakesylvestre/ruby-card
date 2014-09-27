24a6c40d639cc3fa63749ef1cb6b34f08b3afb97
Jake Sylvestre <jakesyl@gmail.com>
2014-09-27 17:09:05 -0400

    added anki instead of mnemosyne, better code readbility

--- mnemosyne/mnemosyne/version.py
+++
@@ -1,4 +0,0 @@
-version = "2.3.2-pre"
-
-if __name__ == "__main__":
-    print version
--- mnemosyne/po/update_trans.py
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/python
-
-import os, shutil
-
-for filename in os.listdir("."):
-    if filename.endswith("po"):
-        print filename
-        os.system ("msgmerge --no-fuzzy-matching " + filename + " mnemosyne.pot -o " + filename)
